3a20bf3af09e5720ba7ab8a244c2c6d6598116fe
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-09 10:23:40 +0800

    >  compile NEMU 3024244483 root Linux Pluto 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:23:40 up 55 min,  1 user,  load average: 0.03, 0.09, 0.14 fac36cc78903cd3314820227a943d9dbc42450

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,36 +14,47 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-     struct {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-     } gpr[8];
+typedef struct
+{
+	union
+	{
+		// 通用寄存器的数组访问方式
+		struct
+		{
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
-     /* Do NOT change the order of the GPRs' definitions. */
+		//按名字访问的方式
+		struct
+		{
+			uint32_t eax, ecx, edx, ebx;
+			uint32_t esp, ebp, esi, edi;
+		};
+	};
 
-     uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	swaddr_t eip;
 
-     swaddr_t eip;
-     
-     union {
-		struct {
-			uint32_t CF		:1;
-			uint32_t pad0	:1;
-			uint32_t PF		:1;
-			uint32_t pad1	:1;
-			uint32_t AF		:1;
-			uint32_t pad2	:1;
-			uint32_t ZF		:1;
-			uint32_t SF		:1;
-			uint32_t TF		:1;
-			uint32_t IF		:1;
-			uint32_t DF		:1;
-			uint32_t OF		:1;
-			uint32_t IOPL	:2;
-			uint32_t NT		:1;
-			uint32_t pad3	:1;
+	union
+	{
+		struct
+		{
+			uint32_t CF : 1;
+			uint32_t pad0 : 1;
+			uint32_t PF : 1;
+			uint32_t pad1 : 1;
+			uint32_t AF : 1;
+			uint32_t pad2 : 1;
+			uint32_t ZF : 1;
+			uint32_t SF : 1;
+			uint32_t TF : 1;
+			uint32_t IF : 1;
+			uint32_t DF : 1;
+			uint32_t OF : 1;
+			uint32_t IOPL : 2;
+			uint32_t NT : 1;
+			uint32_t pad3 : 1;
 			uint16_t pad4;
 		};
 		uint32_t val;
