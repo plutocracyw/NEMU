1abe0feae82f97e5d8307cee8fda6868861f9d35
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-11 17:18:42 +0800

    >  compile NEMU 3024244483 root Linux Pluto 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:18:42 up  6:08,  1 user,  load average: 0.09, 0.07, 0.02 f6bec36061cb7e88e78ca24cf8bf92da865e7729

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,8 +8,15 @@ typedef struct watchpoint {
 	struct watchpoint *next;
 
 	/* TODO: Add more members if necessary */
-
+	char expr[256];      // 用于存储表达式字符串
+	uint32_t value;      // 用于存储表达式的旧值
 
 } WP;
+
+WP* new_wp();
+void free_wp(WP *wp);
+bool delete_wp(int N);
+void display_watchpoints();
+
 
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,68 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp() {
 
+    Assert(free_ != NULL, "Watchpoint pool is full. Cannot create a new watchpoint.");
+
+    WP *new_node = free_;
+    free_ = free_->next;
+
+    new_node->next = head;
+    head = new_node;
+
+    return new_node;
+}
+
+void free_wp(WP *wp) {
+    if (wp == NULL) {
+        printf("Error: Attempting to free a NULL watchpoint.\n");
+        return;
+    }
+
+    WP *current = head;
+    WP *prev = NULL;
+
+    while (current != NULL && current != wp) {
+        prev = current;
+        current = current->next;
+    }
+
+    Assert(current != NULL, "Attempting to free a watchpoint that is not in use.");
+
+    if (prev == NULL) {
+        head = wp->next;
+    } else {
+        prev->next = wp->next;
+    }
+
+    wp->next = free_;
+    free_ = wp;
+}
+
+bool delete_wp(int N) {
+    WP *wp = head;
+    while (wp != NULL) {
+        if (wp->NO == N) {
+            free_wp(wp);
+            return true;
+        }
+        wp = wp->next;
+    }
+    return false;
+}
+
+
+void display_watchpoints() {
+    if (head == NULL) {
+        printf("No watchpoints set.\n");
+        return;
+    }
+
+    printf("Num\tWhat\t\tOld Value\n");
+    WP *wp = head;
+    while (wp != NULL) {
+        printf("%d\t%s\t\t%u\n", wp->NO, wp->expr, wp->value);
+        wp = wp->next;
+    }
+}
